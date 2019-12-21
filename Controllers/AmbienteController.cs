230005fbd675cc3c3a3a0fc8504091e929712e68
Piero <iepvptr@gmail.com>
2019-12-20 22:16:13 -0500

    creación y primer test

---
+++ Controllers/AmbienteController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using freeEnergyResortAPI.Models;
+
+namespace freeEnergyResortAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class AmbienteController
+    {
+
+        [HttpGet]
+        public List<Ambiente> getAllAmbientes()
+        {
+            List<Ambiente> listAmbientes = new List<Ambiente>();
+
+            Ambiente a1 = new Ambiente
+            {
+                IdAmbiente = 1,
+                CodAmbiente = "abc123",
+                Nombre = "Hotel"
+            };
+
+            Ambiente a2 = new Ambiente
+            {
+                IdAmbiente = 2,
+                CodAmbiente = "abc456",
+                Nombre = "Restaurante"
+            };
+
+            Ambiente a3 = new Ambiente
+            {
+                IdAmbiente = 3,
+                CodAmbiente = "xyz321",
+                Nombre = "Bar el trufo"
+            };
+
+            listAmbientes.Add(a1);
+            listAmbientes.Add(a2);
+            listAmbientes.Add(a3);
+
+            return listAmbientes;
+        }
+    }
+}
---
+++ Models/Ambiente.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace freeEnergyResortAPI.Models
+{
+    public class Ambiente
+    {
+        public int IdAmbiente { get; set; }
+        public string CodAmbiente { get; set; }
+        public string Nombre { get; set; }
+    }
+}
